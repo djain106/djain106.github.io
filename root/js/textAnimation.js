b51ce91fd20c5d3e2b13a2e42d7b03fff74dee9d
Daksh Jain <dakshbjain@gmail.com>
2021-01-09 02:56:20 -0500

    added some stuff

---
+++ root/js/textAnimation.js
@@ -0,0 +1,10 @@
+// Name Animation
+var options = {
+    strings: ['"CODER"', '"ENGINEER"', '"STUDENT"'],
+    typeSpeed: 90,
+    backSpeed: 70,
+    loop: true,
+    smartBackspace: false
+};
+
+var typed = new Typed('#typed', options);
