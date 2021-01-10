4c4fdc0b4ec3d91c45e5ad4f157e1d92ee892d63
Daksh Jain <dakshbjain@gmail.com>
2021-01-10 15:18:05 -0500

    added stuff for loading rendering

--- root/js/index.js
+++ root/js/index.js
@@ -1,68 +1,29 @@
 let mesh, renderer, scene, camera, controls;
+// if (window.DeviceOrientationEvent) {
+//     window.addEventListener("deviceorientation", handleOrientation, true);
+// }
 
-if (window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", handleOrientation, true);
-}
-
-function handleOrientation(event) {
-    var alpha = Math.abs(event.alpha);
-    var beta = Math.abs(event.beta);
-    var gamma = Math.abs(event.gamma);
-    const page = document.body;
-    if (!alpha && !beta && !gamma) {
-        return;
-    }
-    var r = alpha * (255 / 360) % 255, g = beta * (255 / 360) % 255, b = gamma * (255 / 360) % 255;
-    page.style.backgroundColor = `rgb(${r + "," + g + "," + b})`;
-}
+// function handleOrientation(event) {
+//     var alpha = Math.abs(event.alpha);
+//     var beta = Math.abs(event.beta);
+//     var gamma = Math.abs(event.gamma);
+//     const page = document.body;
+//     if (!alpha && !beta && !gamma) {
+//         return;
+//     }
+//     var r = alpha * (255 / 360) % 255, g = beta * (255 / 360) % 255, b = gamma * (255 / 360) % 255;
+//     page.style.backgroundColor = `rgb(${r + "," + g + "," + b})`;
+// }
 
 document.addEventListener('DOMContentLoaded', function () {
-    highlightCode();
-    typeCode();
-    animateBackground();
+    highlight();
+    type();
+    init();
     window.addEventListener('resize', onWindowResize, false);
 }, false);
 
-// Three JS animation
-function animateBackground() {
-    // Renderer
-    renderer = new THREE.WebGLRenderer();
-    const animationDiv = document.getElementById("animation");
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById("animation").appendChild(renderer.domElement);
-
-    // Camera
-    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 500);
-    camera.position.set(0, 0, 100);
-    camera.lookAt(0, 0, 0);
-
-    // Scene
-    scene = new THREE.Scene();
-    const backgroundColor = new THREE.Color("rgb(10, 10, 10)");
-    scene.background = backgroundColor;
-
-    const geometry = new THREE.SphereGeometry(3, 32, 32);
-    const material = new THREE.MeshBasicMaterial({ color: "white" });
-    const sphere = new THREE.Mesh(geometry, material);
-    scene.add(sphere);
-
-    camera.position.z = 20;
-
-    const animate = function () {
-        requestAnimationFrame(animate);
-        camera.rotation.x += 0.02;
-        camera.rotation.y += 0.02;
-        // sphere.rotation.x += 0.01;
-        // sphere.rotation.y += 0.01;
-
-        renderer.render(scene, camera);
-    };
-
-    animate();
-}
-
 // Highlight code block with JS syntax
-function highlightCode() {
+function highlight() {
     hljs.configure({
         tabReplace: ''
     });
@@ -73,7 +34,7 @@ function highlightCode() {
 }
 
 // Typing animation in code block
-function typeCode() {
+function type() {
     var options = {
         strings: ["'PROGRAMMER'", "'ENGINEER'", "'STUDENT'", "'DEVELOPER'"],
         typeSpeed: 90,
@@ -85,6 +46,81 @@ function typeCode() {
     var typed = new Typed('#typed', options);
 }
 
+function activateAnimation() {
+    document.getElementById('animation').style.zIndex = 1;
+}
+
+// Three JS animation
+function init() {
+    // Constants
+    const WIDTH = window.innerWidth, HEIGHT = window.innerHeight;
+
+    // Renderer
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    const animationDiv = document.getElementById("animation");
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.toneMappingExposure = 30;
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.setSize(WIDTH, HEIGHT);
+    renderer.physicallyCorrectLights = true;
+    animationDiv.appendChild(renderer.domElement);
+    const pmremGenerator = new THREE.PMREMGenerator(renderer);
+    pmremGenerator.compileEquirectangularShader();
+
+    // Camera
+    camera = new THREE.PerspectiveCamera(50, WIDTH / HEIGHT, 0.01, 500);
+    camera.position.set(0, 10, 0);
+    camera.lookAt(0, 0, 0);
+
+    // Scene
+    scene = new THREE.Scene();
+    const backgroundColor = new THREE.Color("rgb(10, 10, 10)");
+    scene.background = backgroundColor;
+
+    const loader = new THREE.GLTFLoader();
+
+    loader.load('root/assets/room.glb', function (gltf) {
+        const scale = new THREE.Vector3(10, 10, 10);
+        gltf.scene.scale.set(scale.x, scale.y, scale.z);
+        scene.add(gltf.scene);
+        render();
+        document.getElementById('activate_animation').style.display = "inline";
+        // console.log(gltf.cameras[1]);
+        // camera = gltf.cameras[1].clone();
+    }, undefined, function (error) {
+        console.error(error);
+    });
+
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    // controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+    controls.autoRotate = true;
+    controls.dampingFactor = 0.05;
+    controls.screenSpacePanning = false;
+    controls.minDistance = 0;
+    controls.maxDistance = 500;
+    controls.update();
+
+    controls.addEventListener('change', render);
+    render();
+    // camera.position.z = 20;
+
+    // const animate = function () {
+    //     requestAnimationFrame(animate);
+    //     sphere.rotation.x += 0.01;
+    //     sphere.rotation.y += 0.01;
+
+    //     renderer.render(scene, camera);
+    // };
+
+    // animate();
+}
+
+function render() { // to allow for very bright scenes.
+    renderer.shadowMap.enabled = true;
+    renderer.render(scene, camera);
+}
+
 function onWindowResize() {
 
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -94,5 +130,4 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     render();
-
 }
